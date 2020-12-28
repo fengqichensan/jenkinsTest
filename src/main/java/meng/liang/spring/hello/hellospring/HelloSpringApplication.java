a3e5866aef3fdafd476fd29eb969702b47fa681f
Liang Meng <meng.liang@accenture.com>
2020-12-28 20:11:42 +0800

    nothing

--- src/main/java/meng/liang/spring/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/meng/liang/spring/hello/hellospring/HelloSpringApplication.java
@@ -2,9 +2,11 @@ package meng.liang.spring.hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestControler
+@RestController
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
